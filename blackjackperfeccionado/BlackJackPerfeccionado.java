634a33629bcf419dc77cbf8ef0aff121fc56f802
Ber <bernardofoco@gmail.com>
2021-10-13 10:31:32 -0300

    Segundo commit. inclusion boolean gano

--- blackjackperfeccionado/BlackJackPerfeccionado.java
+++ blackjackperfeccionado/BlackJackPerfeccionado.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package blackjackperfeccionado;
 
 import java.util.Scanner;
 
-/**
- *
- * @author berna
- */
+/* @author berna */
 public class BlackJackPerfeccionado {
-
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         
         
@@ -25,7 +15,7 @@ public class BlackJackPerfeccionado {
         
         int pideCartas, queCartaEs, nuevaMano;
         float mazos,ultimaCartaBanca, probBlancas, probNegras, cantidadCartasTotal, cantidadCartasMano, jugadores, cartasNegras, cartasBlancas, negrasEnMesa, blancasEnMesa;
-        
+        boolean gano;
         
         System.out.println("Cuantos mazos usaran?");
         
